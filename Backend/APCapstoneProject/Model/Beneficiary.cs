ee43b241d8da5b080754d60015a84c018d371001
Chingapo <arjunpareek03@gmail.com>
2025-10-18 14:13:35 +0530

    Added new models

---
+++ Backend/APCapstoneProject/Model/Beneficiary.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APCapstoneProject.Model
+{
+    public class Beneficiary
+    {
+        public int BeneficiaryId { get; set; }
+
+        [Required(ErrorMessage = "clientId is Required!")]
+        public int ClientId { get; set; }
+
+
+        [Required(ErrorMessage = "Beneficiary name is Required!")]
+        public string BeneficiaryName { get; set; }
+
+
+        [Required(ErrorMessage = "AccountNumber is Required!")]
+        public string AccountNumber { get; set; }
+
+
+        [Required(ErrorMessage = "Bank name is Required!")]
+        public string BankName { get; set; }
+
+
+        [Required(ErrorMessage = "IFSC Code is Required!")]
+        public string IFSC { get; set; }
+
+        public bool IsVerified { get; set; } = false;
+
+
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
