4a2337fa71d790516507034fe781db31960d6ffc
Pranav Batki <pranavbatki4@gmail.com>
2025-10-28 10:09:53 +0530

    updated

---
+++ Backend/APCapstoneProject/DTO/Beneficiary/BeneficiaryReadDto.cs
@@ -0,0 +1,13 @@
+﻿namespace APCapstoneProject.DTO.Beneficiary
+{
+    public class BeneficiaryReadDto
+    {
+        public int BeneficiaryId { get; set; }
+        public int ClientUserId { get; set; }
+        public string BeneficiaryName { get; set; }
+        public string AccountNumber { get; set; }
+        public string BankName { get; set; }
+        public string IFSC { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Backend/APCapstoneProject/DTO/Employee/EmployeeReadDto.cs
@@ -0,0 +1,17 @@
+﻿namespace APCapstoneProject.DTO.Employee
+{
+    public class EmployeeReadDto
+    {
+        public int EmployeeId { get; set; }
+        public int ClientUserId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Email { get; set; }
+        public string AccountNumber { get; set; }
+        public string BankName { get; set; }
+        public string IFSC { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime DateOfJoining { get; set; }
+        public DateTime? DateOfLeaving { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
